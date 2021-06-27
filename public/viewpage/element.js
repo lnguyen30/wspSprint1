88bfd609e4de0e293b5da2dddf06f71c03d2eaca
Long Nguyen <lnguyen30@uco.edu>
2021-06-26 23:52:10 -0500

    navbar, sign in modal set up

---
+++ public/viewpage/element.js
@@ -0,0 +1,9 @@
+//top menu bar
+export const menuSignIn = document.getElementById('menu-signin');
+export const menuHome = document.getElementById('menu-home');
+export const menuPurchases = document.getElementById('menu-purchases');
+export const menuSignOut = document.getElementById('menu-signout');
+export const menuCart = document.getElementById('menu-cart');
+export const menuProfile = document.getElementById('menu-profile');
+export const menuUsers = document.getElementById('menu-users');
+export const menuProducts = document.getElementById('menu-products');
